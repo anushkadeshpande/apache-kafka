39e21a15d617a42c1fcdd37853b5da30b2f1e80e
anushkadeshpande <anushkaa5000@gmail.com>
2024-02-18 04:31:31 +0530

    Fixed Consumer and Producer codes

--- java/kafka-consumer/src/main/java/com/example/training/kafka_consumer/Consumer.java
+++ java/kafka-consumer/src/main/java/com/example/training/kafka_consumer/Consumer.java
@@ -9,28 +9,31 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class Consumer {
 	public static void main(String args[]){
-		 Properties kafkaProps = new Properties();
-		 kafkaProps.put("bootstrap.servers", "localhost:9094,localhost:9093");
-		 kafkaProps.put("group.id","invalid");
-		 kafkaProps.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-				 kafkaProps.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");		 	 KafkaConsumer<String, String> consumer = new KafkaConsumer<String,String>(kafkaProps);		
-		 consumer.subscribe(Collections.singletonList("test"));
-		 try {
-		      int i=0;
-		      System.out.println("----------- Started Polling For Data ----------- ");
-		      while (i < 2000) {
-		            i++;
-		            ConsumerRecords<String, String> records = consumer.poll(100);
-		              for(ConsumerRecord<String, String> record : records){
-		                   System.out.printf("Partition = %d, offset = %d, key = %s, value = %s%n", 
-						    record.partition(), record.offset(), record.key(), record.value());
-		              }
-		      }
-		      System.out.println("----------- Completed Polling ----------- ");
-		      consumer.close();
-		 } catch (Exception e) {
-		     e.printStackTrace();
-		   }
-		  }
+		Properties kafkaProps = new Properties();
+		kafkaProps.put("bootstrap.servers", "localhost:9093,localhost:9094");
+		kafkaProps.put("group.id","invalid");
+		kafkaProps.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+		kafkaProps.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");		
+		
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String,String>(kafkaProps);		
+		consumer.subscribe(Collections.singletonList("test"));
+		try {
+			int i=0;
+			System.out.println("----------- Started Polling For Data ----------- ");
+			while (i < 2000) {
+				 i++;
+				 ConsumerRecords<String, String> records = consumer.poll(100);				 
+				 for (ConsumerRecord<String, String> record : records){
+					 System.out.printf("Partition = %d, offset = %d, key = %s, value = %s%n", 
+						record.partition(), record.offset(), record.key(), record.value());
+				 }
+			 }
+			 System.out.println("----------- Completed Polling ----------- ");
+			consumer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-}
+		 }
+	 
