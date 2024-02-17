cd989b2b81d82db8b80b62bda71c3f8a1eead834
Anushka <anushkaa5000@gmail.com>
2024-02-18 03:42:24 +0530

    Added java example for Producer and Consumer

---
+++ kafka-consumer/src/main/java/com/example/training/kafka_consumer/Consumer.java
@@ -0,0 +1,36 @@
+package com.example.training.kafka_consumer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class Consumer {
+	public static void main(String args[]){
+		 Properties kafkaProps = new Properties();
+		 kafkaProps.put("bootstrap.servers", "localhost:9094,localhost:9093");
+		 kafkaProps.put("group.id","invalid");
+		 kafkaProps.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+				 kafkaProps.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");		 	 KafkaConsumer<String, String> consumer = new KafkaConsumer<String,String>(kafkaProps);		
+		 consumer.subscribe(Collections.singletonList("test"));
+		 try {
+		      int i=0;
+		      System.out.println("----------- Started Polling For Data ----------- ");
+		      while (i < 2000) {
+		            i++;
+		            ConsumerRecords<String, String> records = consumer.poll(100);
+		              for(ConsumerRecord<String, String> record : records){
+		                   System.out.printf("Partition = %d, offset = %d, key = %s, value = %s%n", 
+						    record.partition(), record.offset(), record.key(), record.value());
+		              }
+		      }
+		      System.out.println("----------- Completed Polling ----------- ");
+		      consumer.close();
+		 } catch (Exception e) {
+		     e.printStackTrace();
+		   }
+		  }
+
+}
---
+++ kafka-producer/src/test/java/com/example/training/kafka_producer/AppTest.java
@@ -0,0 +1,20 @@
+package com.example.training.kafka_producer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
